30e5400313ea38513743972ea21a6a1a01a1ce26
Shailee Patel <shaileeee@gmail.com>
2020-11-02 11:30:18 -0800

    server connected

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+ReactDOM.render(<App/>, document.getElementByID('app'));
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var PORT = 3000;
 
+var publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
 
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+});
