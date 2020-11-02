4419977aaeaee0c85b64ac61726677ea30e94af2
Shailee Patel <shaileeee@gmail.com>
2020-11-02 14:05:47 -0800

    add checkout component with a onclick listerner

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+var Checkout = (props) => {
+  return (
+    // create a button
+    <button onClick = {props.clickCheckout}>Checkout</button>
+    // create a click listener
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this.clickCheckout = this.clickCheckout.bind(this);
+  }
+
+  clickCheckout(event){
+    console.log('clicked');
+  }
+
+  render() {
+    return (
+      <div>
+        <Checkout clickCheckout = {this.clickCheckout}/>
+        {/* <Form1 />
+        <Form2 />
+        <Form3 />
+        <Purchase /> */}
+      </div>
+    );
+  }
+}
 
 
-
-
-ReactDOM.render(<App/>, document.getElementByID('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
 
 
