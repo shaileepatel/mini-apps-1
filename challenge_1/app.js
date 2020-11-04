b6a0070239c0eeb6a38ed737ed825a3564dac344
Shailee Patel <shaileeee@gmail.com>
2020-11-03 23:18:32 -0800

    Challenge_1 - Final refactor code

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,50 +1,68 @@
-var tiles = document.getElementsByTagName("td");
-var decision = document.getElementById("decision");
-var restart = document.getElementById('restart')
-var pairs = [
-  [tiles[0], tiles[1], tiles[2]],
-  [tiles[3], tiles[4], tiles[5]],
-  [tiles[6], tiles[7], tiles[8]],
-  [tiles[0], tiles[3], tiles[6]],
-  [tiles[1], tiles[4], tiles[7]],
-  [tiles[2], tiles[5], tiles[8]],
-  [tiles[0], tiles[4], tiles[8]],
-  [tiles[2], tiles[4], tiles[6]],
-]
-var count = 0;
-var endGame = false;
+var tiles;
+var decision;
+var restart;
+var pairs;
+var count;
+var endGame;
 
-for (var i = 0; i < tiles.length; i++) {
-  tiles[i].addEventListener("click", (event) => {
-    if (event.target.textContent) {
-      return;
-    }
-    if (endGame) {
-      return;
-    }
-    var currentPlayer = (count % 2 === 0 ? 'X' : 'O');
-    event.target.textContent = currentPlayer;
-    count++;
-    if (didWin()) {
-      endGame = true;
-      decision.textContent = `${currentPlayer} wins!`;
-      return;
-    }
-    if (count === 9) {
-      endGame = true;
-      decision.textContent = 'Its a tie!';
-    }
-  });
+init();
+
+function init() {
+  tiles = document.getElementsByTagName("td");
+  decision = document.getElementById("decision");
+  restart = document.getElementById('restart');
+  pairs = [
+    [tiles[0], tiles[1], tiles[2]],
+    [tiles[3], tiles[4], tiles[5]],
+    [tiles[6], tiles[7], tiles[8]],
+    [tiles[0], tiles[3], tiles[6]],
+    [tiles[1], tiles[4], tiles[7]],
+    [tiles[2], tiles[5], tiles[8]],
+    [tiles[0], tiles[4], tiles[8]],
+    [tiles[2], tiles[4], tiles[6]],
+  ];
+  count = 0;
+  endGame = false;
+
+  eventListeners();
 }
 
-restart.addEventListener("click", (event) => {
+function eventListeners () {
+  for (var i = 0; i < tiles.length; i++) {
+    tiles[i].addEventListener("click", playTurn);
+  }
+  restart.addEventListener("click", resetGame)
+}
+
+function playTurn (event) {
+  if (event.target.textContent) {
+    return;
+  }
+  if (endGame) {
+    return;
+  }
+  var currentPlayer = (count % 2 === 0 ? 'X' : 'O');
+  event.target.textContent = currentPlayer;
+  count++;
+  if (didWin()) {
+    endGame = true;
+    decision.textContent = `${currentPlayer} wins!`;
+    return;
+  }
+  if (count === 9) {
+    endGame = true;
+    decision.textContent = 'Its a tie!';
+  }
+}
+
+function resetGame(event) {
   count = 0;
   endGame = false;
   for (var i = 0; i < tiles.length; i++) {
     tiles[i].textContent = '';
   }
   decision.textContent = '';
-})
+}
 
 function didWin () {
   for(var i = 0; i < pairs.length; i++) {
